d0d7452c60d098e09c78ff8158b5e1535a72b6ad
WoolenLamb2171 <85489253+WoolenLamb2171@users.noreply.github.com>
2023-01-08 16:50:09 +0300

    Update Container.js

--- src/Container.js
+++ src/Container.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Container({ children }) {
+export function Container({ children }) {
   return <div className="ui-container">{children}</div>;
 }
