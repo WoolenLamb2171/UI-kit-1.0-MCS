317a270f7857946a0de4fe4ea33fad3ceb237244
WoolenLamb2171 <85489253+WoolenLamb2171@users.noreply.github.com>
2023-01-08 16:06:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
 import "./index.css";
+import { Button } from "./Button.js";
+import { Container } from "./Container.js";
+import { Input } from "./Input.js";
+import { Link } from "./Link.js";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Container>
+        <Input placeholder="test" />
+        <Button type="кнопка">Не нажимай</Button>
+        <Link href="pornhub.com">НЕ ЗАХОДИ</Link>
+      </Container>
+    </>
+  );
 }
 
 render(<App />, document.querySelector("#root"));
+
